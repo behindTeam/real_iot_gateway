a0e2b80171d8a6e02554858058927b2249b090c0
kkh5535 <kkh5535@naver.com>
2023-11-30 14:06:23 +0900

    node 60

--- src/main/java/com/front/Main2.java
+++ src/main/java/com/front/Main2.java
@@ -4,13 +4,15 @@ import org.json.simple.JSONObject;
 
 import com.front.message.JsonMessage;
 import com.front.message.Message;
+import com.front.message.StringArrayMessage;
+import com.front.wire.BufferedWire;
+import com.front.wire.Wire;
 
 public class Main2 {
     public static void main(String[] args) {
-        JSONObject object = new JSONObject();
-        object.put("a", "A");
-        object.put("b", "B");
-        Message jsonMessage = new JsonMessage(object);
-        System.out.println(jsonMessage.toString());
+        Message argMessage = new StringArrayMessage(args);
+        Wire wire1 = new BufferedWire();
+        wire1.put(argMessage);
+
     }
 }
