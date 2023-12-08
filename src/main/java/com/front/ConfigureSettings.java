54be5081d2b86d2ff723a2d4fa5fdddfd380cc2f
kkh5535 <kkh5535@naver.com>
2023-12-08 15:04:26 +0900

    save

---
+++ src/main/java/com/front/ConfigureSettings.java
@@ -0,0 +1,14 @@
+package com.front;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class ConfigureSettings {
+    JSONObject object;
+
+    public ConfigureSettings(JSONObject object) {
+        this.object = object;
+    }
+
+    JSONArray array = object.
+}
